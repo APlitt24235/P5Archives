18b52e7c164c987482c46a754ce60b2b55048ccb
APlitt24235 <Alexa24235@live.ytech.edu>
2023-02-16 10:54:17 -0500

    Added trebuc Font and Text Display (DGRPVN)

--- DanganronpaVN/game.py
+++ DanganronpaVN/game.py
@@ -22,6 +22,14 @@ byakuya_image = pygame.image.load("byakuya1.png")
 kyoko_position = (0, -500)
 byakuya_position = (300, -250)
 
+#Load font
+font = pygame.font.Font("trebuc.ttf", 36)
+
+#Define text
+text = "It's been 5 years since the incident."
+text_image = font.render(text, True, (255, 255, 255))
+text_position = (0, 450)
+
 #Run the game loop
 running = True
 while running:
@@ -33,6 +41,7 @@ while running:
     screen.blit(background_image, (-75, 25))
     screen.blit(kyoko_image, kyoko_position)
     screen.blit(byakuya_image, byakuya_position)
+    screen.blit(text_image, text_position)
     pygame.display.update()
 
 #Quit pygame
